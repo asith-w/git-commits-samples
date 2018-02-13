c6c87b9cc6fb5bf49bfdd2ddbf4556455d3e8a6a
asith-w <asithgit@gmail.com>
2018-02-13 16:08:01 +0530

    IN

---
+++ skyux-spa-sample-app/src/main.ts
@@ -0,0 +1,12 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppExtrasModule } from './app/app-extras.module';
+
+declare const Office: any;
+
+ Office.initialize = function () {
+  platformBrowserDynamic().bootstrapModule(AppExtrasModule);
+ };
+
+
